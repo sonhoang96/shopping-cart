365520c6bf4645a44bb3dd94aa4780940305de79
sonhoang96 <sonhoangit96@gmail.com>
2020-05-18 23:30:05 +0700

    update code

--- js/index.js
+++ js/index.js
@@ -3,13 +3,11 @@ $(document).ready(function() {
 	//window scroll
 	const heightBar = parseInt($('nav').css('height'));
 
-	$('a.nav-link').bind('click', function(event) {
+	$('a.nav-link, .change-col-left a').bind('click', function(event) {
 		event.preventDefault();
 		$('html').stop().animate({
 			scrollTop: $($(this).attr('href')).offset().top - heightBar + 1
 		}, 1000);
-		$('.nav-item.active').removeClass('active');
-		$(this).parent().addClass('active');
 	});//setup click navbar link to scroll to true position
 
 	$(window).scroll(function(){
@@ -39,28 +37,92 @@ $(document).ready(function() {
 		$(this).addClass('active');
 		if($(this).attr('id') === 'cruiser'){
 			$('#explore .row').css({display: 'flex'})
-			$('.row.sport').css({display: 'none'});
-			$('.row.vespa').css({display: 'none'});
+			$('div[moto*="vespa"]').css({display: 'none'});
+			$('div[moto*="sport"]').css({display: 'none'});
 		}
 		if($(this).attr('id') === 'sport'){
 			$('#explore .row').css({display: 'flex'})
-			$('.row.cruiser').css({display: 'none'});
-			$('.row.vespa').css({display: 'none'});		
+			$('div[moto*="cruiser"]').css({display: 'none'});
+			$('div[moto*="vespa"]').css({display: 'none'});		
 		}
 		if($(this).attr('id') === 'vespa'){
 			$('#explore .row').css({display: 'flex'})
-			$('.row.cruiser').css({display: 'none'});
-			$('.row.sport').css({display: 'none'});
+			$('div[moto*="cruiser"]').css({display: 'none'});
+			$('div[moto*="sport"]').css({display: 'none'});
 		}
 		if($(this).attr('id') === 'all'){
-			$('#explore .row').css({display: 'flex'})
+			$('#explore div[moto]').css({display: 'flex'});
 		}
-		console.log($(this).attr('id'))
 	});
 	//setup button change type of moto
 
 	$('#cart').bind('click', function(event){
 		event.preventDefault();
 		$('#list').toggleClass('open-list');
-	})
+		$('#cart').toggleClass('active');
+	});
+	//turn on-off list-board-cart 
+
+	//<--------------------------------------------------------------------------------------------------------------->
+	//setup shopping-cart-items
+
+	let INVENTORY = [];
+	let id = 0;
+	$('i.fa-shopping-cart').bind('click', function(event) {
+		event.preventDefault();
+		const cardItem = $(this).parentsUntil('.row').find('.card');
+		const nameCard = $(cardItem.find('.card-body h4')).text();
+		const imageCard = $(cardItem.find('img')).attr('src');
+		const priceCard = $(cardItem.find('.cast')).text();
+
+		if(INVENTORY.find(item => item.name === nameCard)){
+			$('.name-item').text(nameCard);
+			$('div.alert.alert-secondary').css('display', 'block');
+			setTimeout(() => {
+				$('div.alert.alert-secondary').css('display', 'none');
+			}, 5000);
+			//if item was in cart, alert-board is going to be appearance
+		}else{
+			addItem(nameCard,id,imageCard,priceCard);
+			INVENTORY.push({
+				name  : nameCard,
+				id 	  : id,
+				image : imageCard,
+				price : priceCard
+			});
+			id++;
+		}
+	});
+
+	const addItem = (name,id,image,price) => {
+		let item = `<li class="list-group-item" id='${id}'>
+						<div class="row">
+							<div class="col-3 change-item-image">
+								<img src="${image}" alt="${name}" class="w-100 shopping-cart-image">
+							</div>
+							<div class="col-6">
+								<p class="shopping-cart-name">${name}</p>
+							</div>
+							<div class="col-3 change-price">	
+								<p class="price">${price}</p>
+								<span class="symbol-price">K</span>
+							</div>	
+						</div>
+						<div class="row">
+							<div class="col-8 adjust-button-amounts">
+								<input type="number" class="amount" value ="1">
+							</div>
+							<div class="col-4 remove-button">
+								<a href="#" class="btn btn-outline-secondary">remove</a>
+							</div>
+						</div>
+					</li> <!--end list-item-->`
+
+		$('ul.list-group').append(item);
+	}
+	const totalPrice = () => {
+		$('.list-group-item')
+	}
+	//setup shopping-cart-items
+	//<--------------------------------------------------------------------------------------------------------------->
 });
